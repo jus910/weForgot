962cffc7d8f6360fbafaa3c00c6a34116046cc9f
jus910 <58864969+jus910@users.noreply.github.com>
2022-01-14 13:32:32 -0500

    Delete itemTest2 directory

--- itemTest2/Cookie.java
+++
@@ -1,8 +0,0 @@
-public class Cookie extends Item{
-
-    public String effect() {
-        System.out.println("Cookie was consumed");
-        return "HP:10. Atk:10.";
-    }
-    
-}
--- itemTest2/Item.java
+++
@@ -1,6 +0,0 @@
-public class Item {
-    public String effect() {
-        System.out.println("Item was used");
-        return "HP:0. Atk:0. Def:0. Spd:0.";
-    }
-}
