2910244203f5cd9aae718fd857ffe3ed288447e8
Jonathan Song <75910900+jonathansong8@users.noreply.github.com>
2022-01-20 19:54:07 -0500

    Update Monster.java

--- v7/Monster.java
+++ v7/Monster.java
@@ -3,11 +3,11 @@ public class Monster extends Fighter {
     public Monster(int x, int y) {
         super(x, y);
         icon = "M";
-        HP = (Math.random() * ((100 - 50) + 1)) + 50;
-        Atk = (Math.random() * ((100 - 50) + 1)) + 50;
-        Str = (Math.random() * ((2 - 1) + 1));
-        Def = (Math.random() * ((100 - 50) + 1)) + 50;
-        Spd = (Math.random() * ((50 - 25) + 1)) + 25;
+        HP = (int)(Math.random() * ((100 - 50) + 1)) + 50;
+        Atk = (int)(Math.random() * ((100 - 50) + 1)) + 50;
+        Str = (int)(Math.random() * ((2 - 1) + 1));
+        Def = (int)(Math.random() * ((100 - 50) + 1)) + 50;
+        Spd = (int)(Math.random() * ((50 - 25) + 1)) + 25;
     }
 
     public void attack(Fighter opponent) {
